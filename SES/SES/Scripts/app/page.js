3fcbdb32f62367bc9f93a618cad877b6117675f9
Nano213 <thaonx213@gmail.com>
2015-11-22 22:14:32 +0700

    Fix wrong syntax

--- SES/SES/Scripts/app/page.js
+++ SES/SES/Scripts/app/page.js
@@ -35,126 +35,126 @@ $(document).ready(function () {
     //Đơn bán hàng
     $('#menu_OP_SalesOrder').click(function () {
         onLoadPage(r + "/OP_SalesOrder/PartialDetail");
-        //2.Kho
-        $("#menu_IN_WareHouse").click(function () {
-            //debugger;
-            onLoadPage(r + "/IN_WareHouse/PartialWH");
-        });
-        //3.Vi Tri Kho
-        $("#menu_IN_WareHouseLocation").click(function () {
-            //debugger;
-            onLoadPage(r + "/IN_WareHouseLocation/PartialWHL");
-        });
-        //location
-        $("#menu_Location").click(function () {
-            onLoadPage(r + "/Location/PartialLocation");
-        });
-        //Lịch Nghỉ
-        $("#menu_AdminMasterHoliday").click(function () {
-            onLoadPage(r + "/AdminMasterHoliday/TreeView");
-        });
-        //Lý do
-        $("#menu_Reason").click(function () {
-            onLoadPage(r + "/Reason/PartialReason");
-        });
-        // Phân cấp Vùng miền
-        $("#menu_AdminMasterTerritory").click(function () {
-            onLoadPage(r + "/AdminMasterTerritory/PartialOthersTerritory");
-        });
+    });
+    //2.Kho
+    $("#menu_IN_WareHouse").click(function () {
+        //debugger;
+        onLoadPage(r + "/IN_WareHouse/PartialWH");
+    });
+    //3.Vi Tri Kho
+    $("#menu_IN_WareHouseLocation").click(function () {
+        //debugger;
+        onLoadPage(r + "/IN_WareHouseLocation/PartialWHL");
+    });
+    //location
+    $("#menu_Location").click(function () {
+        onLoadPage(r + "/Location/PartialLocation");
+    });
+    //Lịch Nghỉ
+    $("#menu_AdminMasterHoliday").click(function () {
+        onLoadPage(r + "/AdminMasterHoliday/TreeView");
+    });
+    //Lý do
+    $("#menu_Reason").click(function () {
+        onLoadPage(r + "/Reason/PartialReason");
+    });
+    // Phân cấp Vùng miền
+    $("#menu_AdminMasterTerritory").click(function () {
+        onLoadPage(r + "/AdminMasterTerritory/PartialOthersTerritory");
+    });
    
 
-        //Kho ấn phẩm
-        $("#menu_ListPublication").click(function () {
-            onLoadPage(r + "/ListPublication/PartialRole");
-        });
-
-        //Kho và đơn vị tính
-        $("#menu_WareHouse").click(function () {
-            onLoadPage(r + "/WareHouse/PartialWH");
-        });
-        //Mu hàng
-        $('#menu_SalesOrder').click(function () {
-            onLoadPage(r + "/SalesOrder/PartialRole");
-        })
-        //Đơn vị vận chuyển
-        $("#menu_Transporter").click(function () {
-            onLoadPage(r + "/Transporter/PartialTransporter");
-        });
-        $("#menu_DeliveryFee").click(function () {
-            onLoadPage(r + "/DeliveryFee/PartialDeliveryFee");
-        });
-        $("#menu_DeliveryDiscount").click(function () {
-            onLoadPage(r + "/DeliveryDiscount/PartialDeliveryDiscount");
-        });
-        $("#menu_DeliveryPromotion").click(function () {
-            onLoadPage(r + "/DeliveryPromotion/PartialDeliveryPromotion");
-        });
-        $("#menu_RuleCheck").click(function () {
-            onLoadPage(r + "/RuleCheck/PartialRuleCheck");
-        });
-        $("#menu_Printer").click(function () {
-            onLoadPage(r + "/Printer/PartialPrinter");
-        });
-        $("#menu_Contract").click(function () {
-            onLoadPage(r + "/Contract/PartialContract");
-        });
-        $("#menu_Products").click(function () {
-            onLoadPage(r + "/Products/PartialProducts");
-        });
-        $("#menu_DeliveryFormula").click(function () {
-            onLoadPage(r + "/DeliveryFormula/PartialDeliveryFormula");
-        });
-        $("#menu_Customer").click(function () {
-            onLoadPage(r + "/Customer/PartialCustomer");
-        });
-        $("#menu_PO_VendorInfo").click(function () {
-            onLoadPage(r + "/PO_VendorInfo/PartialVendor");
-        });
-        $("#menu_Promotion").click(function () {
-            onLoadPage(r + "/Promotion/PartialPromotion");
-        });
-        var url = localStorage['urlpage'] || '';
-        if (url) {
-            onLoadPage(url);
-        }
-        else {
-            onLoadPage(r + "/Home/Partial");
-        }
+    //Kho ấn phẩm
+    $("#menu_ListPublication").click(function () {
+        onLoadPage(r + "/ListPublication/PartialRole");
     });
 
-    function onLoadPage(url) {
-        $(document).unbind("keypress");
-        $(document).unbind("keydown");
-        eventHotKey = false;
-        if (url != "" && url != null) {
-            $.ajax({
-                url: url,
-                type: "get",
-                beforeSend: function () { $("#divLoading").show(); },
-                success: function (data) {
-                
-                    $("div.k-window").remove();
-                    $("div#content").html(data);
-                    localStorage['urlpage'] = url;
-                    $("#divLoading").hide();
-                },
-           
-                error: function (e) { }            
-            });
-        }
+    //Kho và đơn vị tính
+    $("#menu_WareHouse").click(function () {
+        onLoadPage(r + "/WareHouse/PartialWH");
+    });
+    //Mu hàng
+    $('#menu_SalesOrder').click(function () {
+        onLoadPage(r + "/SalesOrder/PartialRole");
+    })
+    //Đơn vị vận chuyển
+    $("#menu_Transporter").click(function () {
+        onLoadPage(r + "/Transporter/PartialTransporter");
+    });
+    $("#menu_DeliveryFee").click(function () {
+        onLoadPage(r + "/DeliveryFee/PartialDeliveryFee");
+    });
+    $("#menu_DeliveryDiscount").click(function () {
+        onLoadPage(r + "/DeliveryDiscount/PartialDeliveryDiscount");
+    });
+    $("#menu_DeliveryPromotion").click(function () {
+        onLoadPage(r + "/DeliveryPromotion/PartialDeliveryPromotion");
+    });
+    $("#menu_RuleCheck").click(function () {
+        onLoadPage(r + "/RuleCheck/PartialRuleCheck");
+    });
+    $("#menu_Printer").click(function () {
+        onLoadPage(r + "/Printer/PartialPrinter");
+    });
+    $("#menu_Contract").click(function () {
+        onLoadPage(r + "/Contract/PartialContract");
+    });
+    $("#menu_Products").click(function () {
+        onLoadPage(r + "/Products/PartialProducts");
+    });
+    $("#menu_DeliveryFormula").click(function () {
+        onLoadPage(r + "/DeliveryFormula/PartialDeliveryFormula");
+    });
+    $("#menu_Customer").click(function () {
+        onLoadPage(r + "/Customer/PartialCustomer");
+    });
+    $("#menu_PO_VendorInfo").click(function () {
+        onLoadPage(r + "/PO_VendorInfo/PartialVendor");
+    });
+    $("#menu_Promotion").click(function () {
+        onLoadPage(r + "/Promotion/PartialPromotion");
+    });
+    var url = localStorage['urlpage'] || '';
+    if (url) {
+        onLoadPage(url);
     }
+    else {
+        onLoadPage(r + "/Home/Partial");
+    }
+});
 
-    function resetMenu() {
-        $("#menuLeft").find('li').removeClass('active').removeClass('open');
+function onLoadPage(url) {
+    $(document).unbind("keypress");
+    $(document).unbind("keydown");
+    eventHotKey = false;
+    if (url != "" && url != null) {
+        $.ajax({
+            url: url,
+            type: "get",
+            beforeSend: function () { $("#divLoading").show(); },
+            success: function (data) {
+                
+                $("div.k-window").remove();
+                $("div#content").html(data);
+                localStorage['urlpage'] = url;
+                $("#divLoading").hide();
+            },
+           
+            error: function (e) { }            
+        });
     }
-    function resetMenuHomePage() {
-        $("#menuLeft").find('li').removeClass('active').removeClass('open');
-        $("#menuLeft").find('ul').css('display', 'none');
-        $("#menuLeft").find('em').removeClass('fa-minus-square-o').addClass('fa-plus-square-o');
-    }
-    function hideMaskPopup(idMask) {
-        eventHotKey = false;
-        $(idMask).hide();
-        $(idPopup).fadeOut();
-    }
-})
+}
+
+function resetMenu() {
+    $("#menuLeft").find('li').removeClass('active').removeClass('open');
+}
+function resetMenuHomePage() {
+    $("#menuLeft").find('li').removeClass('active').removeClass('open');
+    $("#menuLeft").find('ul').css('display', 'none');
+    $("#menuLeft").find('em').removeClass('fa-minus-square-o').addClass('fa-plus-square-o');
+}
+function hideMaskPopup(idMask) {
+    eventHotKey = false;
+    $(idMask).hide();
+    $(idPopup).fadeOut();
+}
